345f6060f2f3287e5d2203860aa578121bf71d13
alperergul <alper.ergul.97@gmail.com>
2023-02-24 21:29:05 +0300

    clock added

--- 05-Stubs-Spies-Fixture-Clock/cypress/e2e/location.cy.js
+++ 05-Stubs-Spies-Fixture-Clock/cypress/e2e/location.cy.js
@@ -2,6 +2,8 @@
 
 describe("share location", () => {
   beforeEach(() => {
+    cy.clock();
+
     cy.fixture("user-location.json").as("userLocation");
     cy.visit("/").then((win) => {
       cy.get("@userLocation").then((fakePosition) => {
@@ -47,5 +49,9 @@ describe("share location", () => {
     cy.get("@storeLocation").should("have.been.called");
     cy.get('[data-cy="share-loc-btn"]').click();
     cy.get("@getStoredLocation").should("have.been.called");
+    cy.get('[data-cy="info-message"]').should("be.visible");
+    cy.get('[data-cy="info-message"]').should("have.class", "visible");
+    cy.tick(2000);
+    cy.get('[data-cy="info-message"]').should("not.be.visible");
   });
 });
